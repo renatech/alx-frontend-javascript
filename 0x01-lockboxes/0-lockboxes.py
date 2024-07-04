23f0e6dc420b9826e9d31f54827cd3b01ec27ec9
serena <samuelserenaluv@gmail.com>
2024-07-04 10:02:27 +0000

    m

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,24 @@
-#!/usr/bin/python3
-
-"""
-Problem: You have n number of locked boxes in front of you.
-         Each box is numbered sequentially from 0 to n - 1
-         and each box may contain keys to the other boxes.
-Task: Write a method that determines if all the boxes can be opened.
-"""
-
-
 def canUnlockAll(boxes):
-    """
-    Function that checks with boolean value if the list type and
-    length to invoke two for iterations one to traverse the list
-    and the other to compaer if key is idx or not in order to open
-    """
-    if type(boxes) is not list:
-        return False
-    elif (len(boxes)) == 0:
-        return False
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False
-        for idx in range(len(boxes)):
-            boxes_checked = k in boxes[idx] and k != idx
-            if boxes_checked:
-                break
-        if boxes_checked is False:
-            return boxes_checked
-    return True
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]
+    
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+    
+    return all(opened)
+
+# Test cases
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))  # True
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))  # True
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))  # False
